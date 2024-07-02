83ce875bc045f3788654f055a8e4b7ef90f85074
exo <qiushijsxs@stu.xmu.edu.cn>
2024-07-02 21:02:42 +0800

    aaa

--- liburing/test/nvme.h
+++ liburing/test/nvme.h
@@ -17,26 +17,26 @@ extern "C" {
  * support, use the local version to prevent compilation issues.
  */
 #ifndef CONFIG_HAVE_NVME_URING
-struct nvme_uring_cmd {
-	__u8	opcode;
-	__u8	flags;
-	__u16	rsvd1;
-	__u32	nsid;
-	__u32	cdw2;
-	__u32	cdw3;
-	__u64	metadata;
-	__u64	addr;
-	__u32	metadata_len;
-	__u32	data_len;
-	__u32	cdw10;
-	__u32	cdw11;
-	__u32	cdw12;
-	__u32	cdw13;
-	__u32	cdw14;
-	__u32	cdw15;
-	__u32	timeout_ms;
-	__u32   rsvd2;
-};
+// struct nvme_uring_cmd {
+// 	__u8	opcode;
+// 	__u8	flags;
+// 	__u16	rsvd1;
+// 	__u32	nsid;
+// 	__u32	cdw2;
+// 	__u32	cdw3;
+// 	__u64	metadata;
+// 	__u64	addr;
+// 	__u32	metadata_len;
+// 	__u32	data_len;
+// 	__u32	cdw10;
+// 	__u32	cdw11;
+// 	__u32	cdw12;
+// 	__u32	cdw13;
+// 	__u32	cdw14;
+// 	__u32	cdw15;
+// 	__u32	timeout_ms;
+// 	__u32   rsvd2;
+// };
 
 #define NVME_URING_CMD_IO	_IOWR('N', 0x80, struct nvme_uring_cmd)
 #define NVME_URING_CMD_IO_VEC	_IOWR('N', 0x81, struct nvme_uring_cmd)
